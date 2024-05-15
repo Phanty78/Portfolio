030d8a89cf8dc1f042afd26debea7de282770612
Mael Donnart <maeldonnart@hotmail.fr>
2024-05-15 16:19:30 +0200

    - portfolio V 0.1

---
+++ assets/js/skills.js
@@ -0,0 +1,22 @@
+import { getData } from "./projects.js"
+
+const skillsURL = '/data/skills.json'
+const skillsContainer = document.querySelector(".skills")
+
+getData(skillsURL)
+    .then(SkillsData => {
+        if (SkillsData) {
+            SkillsData.forEach(skill => {
+                if (skill.isVisible === true) {
+                    const skillElement = document.createElement("div")
+                    skillElement.classList.add("skills__skill")
+                    skillElement.textContent = skill.name
+                    skillsContainer.appendChild(skillElement)
+                }
+            });
+        }
+    })
+    .catch(error => {
+        console.error("Error loading skills JSON", error)
+    })
+
